6870b1dd78c52aad3f7cda2c1089202909ce43ff
buskoYYY <busko.y.y.y@gmail.com>
2025-03-21 16:08:25 +0300

    Added player vision

--- Assets/Game/Scripts/InputReader.cs
+++ Assets/Game/Scripts/InputReader.cs
@@ -5,12 +5,19 @@ public class InputReader : MonoBehaviour
 {
     private const string Horizontal = nameof(Horizontal);
     private const string Vertical = nameof(Vertical);
+    private const string MoseX = "Mouse X";
+    private const string MoseY = "Mouse Y";
 
     public event Action<float, float> Moved;
+    public event Action<float, float> Looked;
     void Update()
     {
         float horizontal = Input.GetAxisRaw(Horizontal);
         float vertical = Input.GetAxisRaw(Vertical);
         Moved?.Invoke(horizontal, vertical);
+
+        float mousex = Input.GetAxis(MoseX);
+        float mousey = Input.GetAxis(MoseY);
+        Looked?.Invoke(mousex, mousey);
     }
 }
---
+++ Assets/Game/Scripts/PlayerLooker.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class PlayerLooker : MonoBehaviour
+{
+    [SerializeField] private InputReader _inputReader;
+    [SerializeField] private Camera _camera;
+    [SerializeField] private float _lookSens;
+    [SerializeField] private float _minRotationX;
+    [SerializeField] private float _maxRotationX;
+    private float _rotationX = 0f;
+
+    private void OnEnable()
+    {
+        _inputReader.Looked += LooK;
+    }
+    private void OnDisable()
+    {
+        _inputReader.Looked -= LooK;
+    }
+
+    private void LooK(float horizontal,float vertical)
+    {
+        _rotationX -= vertical * _lookSens * Time.deltaTime;
+        float rotationYDelta = horizontal * _lookSens * Time.deltaTime;
+        
+        _rotationX = Mathf.Clamp(_rotationX,_minRotationX, _maxRotationX);
+
+        _camera.transform.localRotation = Quaternion.Euler(_rotationX,0,0);
+        transform.Rotate(Vector3.up * rotationYDelta);
+    }
+}
